0a10682d00bc62ee4d97f95c5bf11bf7743bccd4
Dangiabhi <94781036+Dangiabhi@users.noreply.github.com>
2022-09-04 05:16:25 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './Navbar'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar home="Home"/>
+     </div>
+  );
+}
+
+export default App;
